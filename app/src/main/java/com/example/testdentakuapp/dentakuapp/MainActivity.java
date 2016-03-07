408e5ea67a63b81e94d94d74b3a0df4e892c47ad
Luxeys <luxeys@Luxeys-no-MacBook-Pro.local>
2016-03-07 14:43:56 +0900

    first commit

---
+++ app/src/main/java/com/example/testdentakuapp/dentakuapp/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.testdentakuapp.dentakuapp;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+
+    Button mButton[];
+
+    int mID[] = { R.id.No0, R.id.No1, R.id.No2, R.id.No3, R.id.No4, R.id.No5, R.id.No6, R.id.No7,
+            R.id.No8, R.id.No9, R.id.NoPlus, R.id.NoMinus, R.id.NoMultiply, R.id.NoBleak,
+            R.id.NoCancel, R.id.NoPoint, R.id.Noequal};
+
+
+    private final int KEY_0 = 0;
+    private final int KEY_1 = 1;
+    private final int KEY_2 = 2;
+    private final int KEY_3 = 3;
+    private final int KEY_4 = 4;
+    private final int KEY_5 = 5;
+    private final int KEY_6 = 6;
+    private final int KEY_7 = 7;
+    private final int KEY_8 = 8;
+    private final int KEY_9 = 9;
+    private final int KEY_PLUS = 10;
+    private final int KEY_MINUS = 11;
+    private final int KEY_MULTIPLY = 12;
+    private final int KEY_BLEAK = 13;
+    private final int KEY_CANCEL = 14;
+    private final int KEY_POINT = 15;
+    private final int KEY_EQUAL = 16;
+
+
+    TextView mTextView;
+
+    int beforeStatus = 0;                                                                           // 前の処理
+
+    int total = 0;                                                                                  // 合計
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.number);
+
+        mButton = new Button[mID.length];
+
+        for (int i = 0; i < mID.length; i++) {
+           mButton[i] = (Button) findViewById(mID[i]);
+           mButton[i].setOnClickListener(this);
+
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        for (int i = 0; i < mID.length; i++) {
+            if (view.equals(mButton[i])){
+                if (i == KEY_CANCEL) {
+                    mTextView.setText("");
+                    total = 0;
+                    beforeStatus = KEY_CANCEL;
+                } else if (i < 10) {
+                    String nowValue = mTextView.getText().toString();
+                    nowValue = nowValue + i;
+                    mTextView.setText(nowValue);
+                    beforeStatus = i;
+                }
+                break;
+            }
+        }
+    }
+}
