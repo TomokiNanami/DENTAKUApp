6ddec0599d15f16cf2525cd1d54aa91ba9282c54
Tomoki Nanami <s13a109y@kokusikan.ac.jp>
2016-12-10 22:04:57 +0900

    add MainActivityViewModel

--- app/src/main/java/com/example/testdentakuapp/dentakuapp/MainActivity.java
+++ app/src/main/java/com/example/testdentakuapp/dentakuapp/MainActivity.java
@@ -11,35 +11,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     Button mButton[];
 
-    int mID[] = { R.id.No0, R.id.No1, R.id.No2, R.id.No3, R.id.No4, R.id.No5, R.id.No6, R.id.No7,   
+    int mID[] = { R.id.No0, R.id.No1, R.id.No2, R.id.No3, R.id.No4, R.id.No5, R.id.No6, R.id.No7,   // XMLファイル上の各ボタンのID付け
             R.id.No8, R.id.No9, R.id.NoPlus, R.id.NoMinus, R.id.NoMultiply, R.id.NoBleak,
             R.id.NoCancel, R.id.NoPoint, R.id.Noequal, R.id.No000,  R.id.No001, R.id.No002,
             R.id.No003, R.id.No004, R.id.No005,};
 
-
-    private final int KEY_0 = 0;
-    private final int KEY_1 = 1;
-    private final int KEY_2 = 2;
-    private final int KEY_3 = 3;
-    private final int KEY_4 = 4;
-    private final int KEY_5 = 5;
-    private final int KEY_6 = 6;
-    private final int KEY_7 = 7;
-    private final int KEY_8 = 8;
-    private final int KEY_9 = 9;
-    private final int KEY_PLUS = 10;
-    private final int KEY_MINUS = 11;
-    private final int KEY_MULTIPLY = 12;
-    private final int KEY_BLEAK = 13;
-    private final int KEY_CANCEL = 14;
-    private final int KEY_POINT = 15;
-    private final int KEY_EQUAL = 16;
-    private final int KEY_001 = 17;
-    private final int KEY_002 = 18;
-    private final int KEY_003 = 19;
-    private final int KEY_004 = 20;
-    private final int KEY_005 = 21;
-
+    private final int KEY_CANCEL = 14;                                                              // それぞれのボタンに数字をつける
 
     TextView mTextView;
 
@@ -52,7 +29,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.number);
+        mTextView = (TextView) findViewById(R.id.number);                                           // XMLのnumberってTextViewを取得
+        mTextView.setText(String.valueOf(0));                                                       // 実行後最初に表示される文字・数字
 
         mButton = new Button[mID.length];
 
@@ -66,7 +44,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         for (int i = 0; i < mID.length; i++) {
-            if (view.equals(mButton[i])){
+            if (view.equals(mButton[i])){                                                           // Cボタンの処理
                 if (i == KEY_CANCEL) {
                     mTextView.setText("");
                     total = 0;
---
+++ app/src/main/java/com/example/testdentakuapp/dentakuapp/MainActivityViewModel.java
@@ -0,0 +1,8 @@
+package com.example.testdentakuapp.dentakuapp;
+
+/**
+ * Created by nanami on 2016/12/10.
+ */
+
+public class MainActivityViewModel {
+}
