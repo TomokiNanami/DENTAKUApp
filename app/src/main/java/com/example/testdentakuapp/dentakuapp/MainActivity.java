0374069548dc6403a0ea191a38fb4e853cf87902
Tomoki Nanami <s13a109y@kokusikan.ac.jp>
2017-11-15 00:17:39 +0900

    コメントの修正、データバインディングの削除

--- app/src/main/java/com/example/testdentakuapp/dentakuapp/MainActivity.java
+++ app/src/main/java/com/example/testdentakuapp/dentakuapp/MainActivity.java
@@ -8,43 +8,65 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-
+    // それぞれのボタンに数字をつける
+    private final int KEY_CANCEL = 14;
     Button mButton[];
-
-    int mID[] = { R.id.No0, R.id.No1, R.id.No2, R.id.No3, R.id.No4, R.id.No5, R.id.No6, R.id.No7,   // XMLファイル上の各ボタンのID付け
-            R.id.No8, R.id.No9, R.id.NoPlus, R.id.NoMinus, R.id.NoMultiply, R.id.NoBleak,
-            R.id.NoCancel, R.id.NoPoint, R.id.Noequal, R.id.No000,  R.id.No001, R.id.No002,
-            R.id.No003, R.id.No004, R.id.No005,};
-
-    private final int KEY_CANCEL = 14;                                                              // それぞれのボタンに数字をつける
-
+    int mID[] = {
+            R.id.No0,
+            R.id.No1,
+            R.id.No2,
+            R.id.No3,
+            R.id.No4,
+            R.id.No5,
+            R.id.No6,
+            R.id.No7,
+            R.id.No8,
+            R.id.No9,
+            R.id.NoPlus,
+            R.id.NoMinus,
+            R.id.NoMultiply,
+            R.id.NoBleak,
+            R.id.NoCancel,
+            R.id.NoPoint,
+            R.id.Noequal,
+            R.id.No000,
+            R.id.No001,
+            R.id.No002,
+            R.id.No003,
+            R.id.No004,
+            R.id.No005
+    };
     TextView mTextView;
 
-    int beforeStatus = 0;                                                                           // 前の処理
+    // 前の処理
+    int beforeStatus = 0;
 
-    int total = 0;                                                                                  // 合計
+    // 合計値(SUM)
+    int total = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.number);                                           // XMLのnumberってTextViewを取得
-        mTextView.setText(String.valueOf(0));                                                       // 実行後最初に表示される文字・数字
+        // XMLのnumberってTextViewを取得
+        mTextView = (TextView) findViewById(R.id.number);
+        // 実行後最初に表示される文字・数字
+        mTextView.setText(String.valueOf(0));
 
         mButton = new Button[mID.length];
 
         for (int i = 0; i < mID.length; i++) {
-           mButton[i] = (Button) findViewById(mID[i]);
-           mButton[i].setOnClickListener(this);
-
+            mButton[i] = (Button) findViewById(mID[i]);
+            mButton[i].setOnClickListener(this);
         }
     }
 
     @Override
     public void onClick(View view) {
         for (int i = 0; i < mID.length; i++) {
-            if (view.equals(mButton[i])){                                                           // Cボタンの処理
+            // Cancelボタンの処理
+            if (view.equals(mButton[i])) {
                 if (i == KEY_CANCEL) {
                     mTextView.setText("");
                     total = 0;
--- app/src/main/java/com/example/testdentakuapp/dentakuapp/MainActivityViewModel.java
+++
@@ -1,8 +0,0 @@
-package com.example.testdentakuapp.dentakuapp;
-
-/**
- * Created by nanami on 2016/12/10.
- */
-
-public class MainActivityViewModel {
-}
